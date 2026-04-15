72bd07495668a67fd4b88baa64d72c65920fe3f5
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2026-04-15 15:12:49 +0300

    feat: continued the runServer, now it has the binding of the server socket

--- Server.cpp
+++ Server.cpp
@@ -2,9 +2,20 @@
 
 void Server::runServer()
 {
-	if (_serverfd = socket(AF_INET, SOCK_STREAM, 0) < 0)
+	if ((_serverfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
 		std::cerr << "socket failed" << std::endl;
-		// exit cleanly
+		return;
 	}
+	sockaddr_in address; // address our server will use
+	std::memset(&address, 0, sizeof(address)); // set everything to 0
+	address.sin_family = AF_INET; // set type (IPv4)
+	address.sin_addr.s_addr = INADDR_ANY; //accept connections from any IP on this machine
+	address.sin_port = htons(PORT); // makes sure the port number is understood correctly outside our computer
+	if (bind(_serverfd, (sockaddr *)&address, sizeof(address)) < 0) // assigns address (IP and port) to our socket
+	{
+		std::cerr << "bind failed" << std::endl;
+		return;
+	}
+
 }
