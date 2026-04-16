96a32712f9bf0b34af99bee8c2b59e8847570318
krepo <krepo@student.hive.fi>
2026-04-16 10:09:25 +0300

    feature: added the listening function for runServer. On top of that also made an infinite loop for the accept function to be able to handle multiple clients.

--- Server.cpp
+++ Server.cpp
@@ -7,8 +7,7 @@ void Server::runServer()
 		std::cerr << "socket failed" << std::endl;
 		return;
 	}
-	sockaddr_in address; // address our server will use
-	std::memset(&address, 0, sizeof(address)); // set everything to 0
+	sockaddr_in address = std::memset(&address, 0, sizeof(address)); // address our server will use and set everything to 0
 	address.sin_family = AF_INET; // set type (IPv4)
 	address.sin_addr.s_addr = INADDR_ANY; //accept connections from any IP on this machine
 	address.sin_port = htons(PORT); // makes sure the port number is understood correctly outside our computer
@@ -17,5 +16,21 @@ void Server::runServer()
 		std::cerr << "bind failed" << std::endl;
 		return;
 	}
+	listen(_serverfd, 10); // Listening (backlog = 10 pending connections)
+	std::cout << "Listening on PORT: " << PORT << std::endl;
+	while (1)
+	{
+		_clientfd = accept(_serverfd, NULL, NULL); // accept blocks here until a client connects
 
+		int n = read(_clientfd, _buffer, BUFFER_SIZE -1); // reading what the client sent
+		buffer[n] = '\0';
+		std::cout << "Received: " << _buffer << "\n";
+
+		char *resp = "Hello from server!\n"; // temporary respons to client
+		write(_clientfd, resp, std::strlen(resp));
+
+		close(_clientfd); // Done with this client
+	}
+
+	close(_serverfd);
 }
