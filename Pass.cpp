e72d0492d782d40d2b5ab23948febfdbc1bf477e
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2026-04-20 14:46:16 +0300

    feat: PASS function done for now, added error messages

--- Pass.cpp
+++ Pass.cpp
@@ -2,17 +2,22 @@
 
 void Server::handlePass(Client *client, std::vector<std::string> params)
 {
-	std::string nick = client->getNick();
+	std::string nick = client->getNick().empty() ? "*" : client->getNick(); // if the nick hasn't been set yet, we use a placeholder
+
 	if (client->isRegistered())
 	{
-		client->getSendBuffer() += ""
+		client->getSendBuffer() += "462 " + nick + " :You may not reregister\r\n";
+		return;
 	}
 	if (params.empty())
 	{
-
+		client->getSendBuffer() += "461 " + nick + " PASS :Not enough parameters\r\n";
+		return;
 	}
 	if (params[0] != _password)
 	{
-
+		client->getSendBuffer() += "464 " + nick + " :Password incorrect\r\n";
+		return;
 	}
+	client->setPasswordValidated(true);
 }
