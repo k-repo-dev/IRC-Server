60ea3013fb70b28269c8346dfd311b3759d1ed57
krepo <krepo@student.hive.fi>
2026-04-21 10:37:34 +0300

    Started implementing handleUser() function. It takes in the client and the parameters for the USER command. It checks if the user is already registered, and if so throws the ERR_ALREADYREGISTERED (462) error. On top of that it checks that the correct enough params and if the username is empty, if not done correctly gives ERR_NEEDMOREPARAMS (461) error.
    The NICK command was added to dev. Rebasing dev with that update to this
    USER command branch.

--- UserCommand.cpp
+++ UserCommand.cpp
@@ -1,2 +1,24 @@
 #include "Server.hpp"
 
+void Server::handleUser(Client* client, std::vector<std::string>& params)
+{
+	// ERR_ALREADYREGISTERED (462)
+	// if USER is sent after registration is complete, reject it
+	if (client->isRegistered())
+	{
+		sendToClient(client,
+			":server 462 " + client->getNick() + " :You may not register\r\n");
+		return;
+	}
+
+	// ERR_NEEDMOREPARAMS (461)
+	// needed: <username> <0> <*> <realname> - 4 params
+	// if <username> is empty, treat as missing params
+	if (params.size() < 4 || params[0].empty())
+	{
+		sendToClient(client,
+			":server 461 * USER :Not enough parameters\r\n");
+		return;
+	}
+	
+}
