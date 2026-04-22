33b744e48f2bd6c426e3f4f4e4f0c1958e3ddec2
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2026-04-22 14:56:29 +0300

    feat: kick done, didn't test it because JOIN needed

--- Kick.cpp
+++ Kick.cpp
@@ -7,7 +7,7 @@ void Server::handleKick(Client *client, std::vector<std::string> params)
 	if (params.size() < 2) // needs at least channel and user to kick
 	{
 		sendToClient(client,
-			":server 461 " + client->getNick() + " :Not enough parameters\r\n");
+			":461 " + client->getNick() + " :Not enough parameters\r\n");
 		return;
 	}
 	std::string channelName = params[0];
@@ -15,7 +15,7 @@ void Server::handleKick(Client *client, std::vector<std::string> params)
 	if (_channelList.find(channelName) == _channelList.end()) // look for channel in map to see if it exists
 	{
 		sendToClient(client,
-			":server 403 " + client->getNick() + "" + channelName + " :No such channel\r\n");
+			":403 " + client->getNick() + "" + channelName + " :No such channel\r\n");
 		return;
 	}
 
@@ -23,18 +23,36 @@ void Server::handleKick(Client *client, std::vector<std::string> params)
 	if (!channel->isMember(client))
 	{
 		sendToClient(client,
-			":server 442 " + client->getNick() + "" + channelName + " :You're not on that channel\r\n");
+			":442 " + client->getNick() + "" + channelName + " :You're not on that channel\r\n");
 		return;
 	}
 	if (!channel->isOperator(client))
 	{
 		sendToClient(client,
-			":server 482 " + client->getNick() + "" + channelName + " :You're not channel operator\r\n");
+			":482 " + client->getNick() + "" + channelName + " :You're not channel operator\r\n");
 		return;
 	}
 
-	std::vector<std::string> targets;
-	
+	std::vector<std::string> kicked;
+	std::stringstream stream(params[1]); // put into a stream so we can read from it
+	std::string temp;
+	while (std::getline(stream, temp, ',')) // reads from stream and keeps putting everything up to , into temp (goes until no more ,)
+		kicked.push_back(temp);
 
+	for (size_t i = 0; i < kicked.size(); i++)
+	{
+		Client* target = channel->getMemberByNick(kicked[i]); // we get the client object of the nick we're looking for
+		if (target == nullptr)
+		{
+			sendToClient(client,
+				":441 " + client->getNick() + "" + kicked[i] + "" + channelName + " :They aren't on that channel\r\n");
+			continue; // if we have several targets, we need to try to kick each one
+		}
 
+		std::string kickMess = ":" + nick + "!" + client->getUserName() + "@localhost KICK " + channelName + " " + kicked[i] + " :" + reasonToKick + "\r\n";
+		const std::unordered_map<int, Client*>& members = channel->getMembers();
+		for (auto it = members.begin(); it != members.end(); it++)
+			sendToClient(it->second, kickMess);
+		channel->removeMember(target);
+	}
 }
