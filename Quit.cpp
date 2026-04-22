162ee976aca685bf6cf5b6ca4b5b57e1d5487b83
krepo <krepo@student.hive.fi>
2026-04-22 09:07:16 +0300

    Created handleQuit() member fuction for the QUIT command. Handeling if it is called with params or not.

--- Quit.cpp
+++ Quit.cpp
@@ -1,2 +1,12 @@
 #include "Server.hpp"
-#include "Channel.hpp"
+
+void Server::handleQuit(Client* client, std::vector<std::string>& params)
+{
+	// a reason is always given, even if QUIT is called without params
+	// if empty, then reason is just "Quit: "
+	std::string reason = "Quit: ";
+	if (!params.empty())
+		reason += params[0];
+	
+	
+}
