d903993401a91bcb632c1a9d87ee9d5ef6b0873b
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2026-04-24 14:11:36 +0300

    feat: start of the function

--- Server.hpp
+++ Server.hpp
@@ -59,7 +59,7 @@ class Server
 		void handleTopic(Client* client, std::vector<std::string>& params);
 		void handleJoin(Client* client, std::vector<std::string>& params);
 		void joinChannel(Client* client, std::string& chan, std::string& key);
-
+		void handleInvite(Client *client, std::vector<std::string> params);
 };
 
 enum command{
